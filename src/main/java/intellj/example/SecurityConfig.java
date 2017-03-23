c184a807180bf31c3a1e8a596950ea6abb2a13b5
guinaja <prayoonsuntornkiti@gmail.com>
2017-03-24 02:07:28 +0700

    fixed

--- src/main/java/intellj/example/SecurityConfig.java
+++ src/main/java/intellj/example/SecurityConfig.java
@@ -1,6 +1,8 @@
 package intellj.example;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by prayoon.su on 3/24/2017.
@@ -11,5 +13,4 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
     }
-}
 }

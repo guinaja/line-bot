477c9279aaa0c54b392b851c9a53ad6eb9758786
guinaja <prayoonsuntornkiti@gmail.com>
2017-03-24 02:33:27 +0700

    fixed

--- src/main/java/intellj/example/LinebotApplication.java
+++ src/main/java/intellj/example/LinebotApplication.java
@@ -5,11 +5,13 @@ import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@LineMessageHandler
 public class LinebotApplication {
 
 	public static void main(String[] args) {

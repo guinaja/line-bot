2dbb68d7439610fed8118392643ad4b189d9244d
prayoonsuntornkiti@gmail.com <prayoonsuntornkiti@gmail.com>
2017-03-24 12:10:45 +0700

    init

--- src/main/java/intellj/example/LinebotApplication.java
+++ src/main/java/intellj/example/LinebotApplication.java
@@ -9,17 +9,32 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import java.util.Random;
+
 @SpringBootApplication
 @LineMessageHandler
 public class LinebotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LinebotApplication.class, args);
 	}
-
+	private static  String[] autoReply = {"ไม่ตลก","อะไรหยอออ","เพื่อนเล่นหรอ","งงอะเด้ งงอะเด้" ,"ไม่ว่าง ยุ่งอยู่"};
 	@EventMapping
 	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
 		System.out.println("event: " + event);
-		return new TextMessage(event.getMessage().getText());
+		System.out.println("message: " + event.getMessage());
+		System.out.println("replyToken: " + event.getReplyToken());
+		String message = event.getMessage().getText();
+		String userId = event.getSource().getUserId();
+
+		String replymessage = "";
+		if ("U1e32ce96ac0156063a0b1626a4299c40".equals(userId)){
+			replymessage = "ฮ่องเต้เสด็จแล้ว";
+		}else{
+			Random rand = new Random();
+			int n = rand.nextInt(5) + 1;
+			replymessage = autoReply[n];
+		}
+		return new TextMessage(replymessage);
 	}
 
 	@EventMapping
